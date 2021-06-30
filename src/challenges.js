d19b85206f705650f70c0deab9d2dc81501bb007
Theo Lima <theo.suporte@hotmail.com>
2021-06-29 21:15:23 -0300

    Requisito #6 e #8

--- src/challenges.js
+++ src/challenges.js
@@ -22,17 +22,25 @@ function splitSentence(phrase) {
 
 // Desafio 4
 function concatName(array) {
-  return (array[array.length - 1] + ", " + array[0]);
+  return (array[array.length - 1] + ', ' + array[0]);
 }
 
 // Desafio 5
-function footballPoints(wins,ties) {
-  return((wins * 3) + (ties));
+function footballPoints(wins, ties) {
+  return ((wins * 3) + (ties));
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numeros) {
+  let counter = 0;
+  let emOrdem = numeros.sort((a,b) => a-b)
+  let maiorNumero = emOrdem[emOrdem.length - 1]
+  for (let index = 0; index < numeros.length; index += 1) {
+    if (numeros[index] === maiorNumero) {
+      counter += 1;
+    } 
+  }
+  return counter;
 }
 
 // Desafio 7
@@ -41,8 +49,19 @@ function catAndMouse() {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numeros) {
+  let novoArray = [];
+  for (let number of numeros)
+    if (number % 3 === 0 && number % 5 !== 0) {
+      novoArray.push('fizz');
+    } else if (number % 5 === 0 && number % 3 !== 0) {
+      novoArray.push('buzz');
+    } else if (number % 3 === 0 && number % 5 === 0) {
+      novoArray.push('fizzBuzz');
+    } else if (number % 3 !== 0 && number % 5 !== 0) {
+      novoArray.push('bug!');
+    }
+  return(novoArray);
 }
 
 // Desafio 9
