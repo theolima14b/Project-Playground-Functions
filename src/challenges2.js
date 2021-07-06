878f7e4cb94d7c0585386e4b84e1e3ae3ca27f97
Theo Lima <theo.suporte@hotmail.com>
2021-07-06 12:36:46 -0300

    Inicia Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,25 +17,41 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber(numbers) {
-  if (numbers.length !== 11) {
-    return 'Array com tamanho incorreto.';
-  }
-  function checkPhoneNumber(numbers) {
-    let repeat = 0;
-    for (let x = 0; x < numbers.length; x += 1) {
-      for (let y = 0; y < numbers.length; y += 1) {
-        if (numbers[x] === numbers[y]) {
-          repeat += 1;
-        }
-      }
-      if (repeat > 0) {
-        return ('não é possível gerar um número de telefone com esses valores');
-      }
+function checkZeroNine(numbers) {
+  let returnTrue = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] < 0) {
+      returnTrue += 1;
+    } if (numbers[i] > 9) {
+      returnTrue += 1;
     }
   }
+  if (returnTrue !== 0) {
+    return ('não é possível gerar um número de telefone com esses valores');
+  } return true;
 }
 
+function checkPhoneNumber(numbers) {
+  let repeat = 0;
+  for (let x = 0; x < numbers.length; x += 1) {
+    for (let y = 0; y < numbers.length; y += 1) {
+      if (numbers[x] === numbers[y]) {
+        repeat += 1;
+      }
+    }
+    if (repeat >= 3) {
+      return (false);
+    } repeat = 0;
+  } return (true);
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+function generatePhoneNumber(numbers) {
+  if (checkPhoneNumber(numbers) === true && checkZeroNine(numbers) === true) {
+    return 'Você está conseguindo';
+  }
+}
+console.log(generatePhoneNumber(numbers));
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
