6968597e99efa56b67d88f484e3deb3107dcb12e
Theo Lima <theo.suporte@hotmail.com>
2021-07-06 13:04:42 -0300

    Correçoes

--- src/challenges2.js
+++ src/challenges2.js
@@ -43,13 +43,14 @@ function checkPhoneNumber(numbers) {
   } return (true);
 }
 
-
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
 function generatePhoneNumber(numbers) {
-  if (checkPhoneNumber(numbers) === true && checkZeroNine(numbers) === true) {
+  if (checkPhoneNumber(numbers) === true || checkZeroNine(numbers) === true) {
     return 'Você está conseguindo';
   }
 }
 console.log(generatePhoneNumber(numbers));
+
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
